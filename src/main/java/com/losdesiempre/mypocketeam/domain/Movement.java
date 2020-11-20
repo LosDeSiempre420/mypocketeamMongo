b8265af07bf459257e8c5bcf34fd4718e9983e64
Antonia <anto.nava07@gmail.com>
2020-11-19 21:19:29 -0300

    Primer test funcionando

--- src/main/java/com/losdesiempre/mypocketeam/domain/Movement.java
+++ src/main/java/com/losdesiempre/mypocketeam/domain/Movement.java
@@ -4,60 +4,92 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "movimientos")
 public class Movement {
-    public void setId(int i) {
+    //Variables
+    private int id;
+    private String name;
+    private String type;
+    private String category;
+    private Integer power;
+    private Integer accuracy;
+    private int pp;
+    private String effect;
+
+    public Movement(int id, String name, String type, String category, Integer power, Integer accuracy, int pp, String effect) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.category = category;
+        this.power = power;
+        this.accuracy = accuracy;
+        this.pp = pp;
+        this.effect = effect;
     }
 
-    public void setName(String absorb) {
-    }
+    public Movement() {
 
-    public void setType(String grass) {
-    }
-
-    public void setCategory(String special) {
-    }
-
-    public void setPower(Integer i) {
-    }
-
-    public void setAccuracy(Integer i) {
-    }
-
-    public void setPp(int i) {
-    }
-
-    public void setEffect(String s) {
     }
 
     public int getId() {
-        return 0;
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
-        return "";
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getType() {
-        return "";
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
     public String getCategory() {
-        return "";
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 
     public Integer getPower() {
-        return -1;
+        return power;
+    }
 
+    public void setPower(Integer power) {
+        this.power = power;
     }
 
     public Integer getAccuracy() {
-        return -1;
+        return accuracy;
+    }
+
+    public void setAccuracy(Integer accuracy) {
+        this.accuracy = accuracy;
     }
 
     public int getPp() {
-        return 0;
+        return pp;
+    }
+
+    public void setPp(int pp) {
+        this.pp = pp;
     }
 
     public String getEffect() {
-        return "";
+        return effect;
+    }
+
+    public void setEffect(String effect) {
+        this.effect = effect;
     }
 }
--- src/main/java/com/losdesiempre/mypocketeam/service/MovementServiceImplementation.java
+++ src/main/java/com/losdesiempre/mypocketeam/service/MovementServiceImplementation.java
@@ -1,13 +1,18 @@
 package com.losdesiempre.mypocketeam.service;
 
 import com.losdesiempre.mypocketeam.domain.Movement;
+import com.losdesiempre.mypocketeam.repository.MovementRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class MovementServiceImplementation {
+    @Autowired
+    private MovementRepository movementRepository;
+
     public List<Movement> listAll() {
-        return null;
+        return movementRepository.findAll();
     }
 }
